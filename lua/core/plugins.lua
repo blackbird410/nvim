8077866391f6a2a63472778585e8c7e262909aee
Neil Taison Rigaud <ntsrigaud14@gmail.com>
2025-02-27 08:32:28 +0800

    chore: update config files

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -37,4 +37,9 @@ require("lazy").setup({
     "nvim-telescope/telescope.nvim", tag = "0.1.4",
     dependencies = { "nvim-lua/plenary.nvim" }
   },
+  opts = {
+    ensure_installed = {
+      "pyright",
+    }
+  },
 })
