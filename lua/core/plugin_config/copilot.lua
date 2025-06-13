aaf3b2657da40375f20a45ad33259b7c2b058418
Neil Taison Rigaud <ntsrigaud14@gmail.com>
2025-06-13 14:12:59 +0800

    chore: update copilot config file
    The added changes involves using the new plugin and added keybindings
    for turning copilot on and off.

--- lua/core/plugin_config/copilot.lua
+++ lua/core/plugin_config/copilot.lua
@@ -1,1 +1,44 @@
-vim.cmd[[highlight CopilotSuggestion ctermfg=8 guifg=white guibg=#5c6370]]
+-- Highlight Copilot ghost text
+vim.cmd([[highlight CopilotSuggestion ctermfg=8 guifg=white guibg=#5c6370]])
+
+-- Setup Copilot plugin
+require("copilot").setup({
+	suggestion = {
+		enabled = true,
+		auto_trigger = true,
+		debounce = 75,
+		keymap = {
+			accept = "<M-l>",
+			next = "<M-]>",
+			prev = "<M-[>",
+			dismiss = "<C-]>",
+		},
+	},
+	panel = { enabled = false },
+})
+
+-- User commands to control Copilot
+vim.api.nvim_create_user_command("CopilotEnable", function()
+	-- Force reattach in case Copilot is not yet started
+	local copilot = require("copilot")
+	copilot.setup({ -- optional: reapply settings
+		suggestion = { auto_trigger = true },
+		panel = { enabled = false },
+	})
+
+	require("copilot.suggestion").toggle_auto_trigger(true)
+	vim.notify("Copilot enabled", vim.log.levels.INFO)
+end, {})
+
+vim.api.nvim_create_user_command("CopilotDisable", function()
+	require("copilot.suggestion").toggle_auto_trigger(false)
+	vim.notify("Copilot disabled", vim.log.levels.WARN)
+end, {})
+
+-- Optional: keybinding to toggle Copilot on/off
+local copilot_enabled = true
+vim.keymap.set("n", "<leader>ct", function()
+	copilot_enabled = not copilot_enabled
+	require("copilot.suggestion").toggle_auto_trigger(copilot_enabled)
+	vim.notify("Copilot " .. (copilot_enabled and "enabled" or "disabled"))
+end, { desc = "Toggle Copilot", silent = true })
