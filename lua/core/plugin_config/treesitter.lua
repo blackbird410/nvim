6407d19a26fa4f2da6f1c1b09dcabffb8f142f73
Neil Taison Rigaud <ntsrigaud14@gmail.com>
2025-06-13 14:28:12 +0800

    feat: add parser ensure install statement for Java and other supplementary languages

--- lua/core/plugin_config/treesitter.lua
+++ lua/core/plugin_config/treesitter.lua
@@ -1,14 +1,29 @@
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all"
-  ensure_installed = { "c", "lua", "rust", "ruby", "vim", "html", "python", "cpp", "css", "go" },
+require("nvim-treesitter.configs").setup({
+	-- A list of parser names, or "all"
+	ensure_installed = {
+		"c",
+		"lua",
+		"rust",
+		"ruby",
+		"vim",
+		"html",
+		"python",
+		"cpp",
+		"css",
+		"go",
+		"java",
+		"bash",
+		"markdown",
+		"markdown_inline",
+	},
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
-  },
-  indent = {
-    enable = true,
-  },
-}
+	-- Install parsers synchronously (only applied to `ensure_installed`)
+	sync_install = false,
+	auto_install = true,
+	highlight = {
+		enable = true,
+	},
+	indent = {
+		enable = true,
+	},
+})
